200fe90c2244b282822c779282db9fec7617cef1
cuong19962012 <tancuong.it@gmail.com>
2023-08-31 10:06:09 +0700

    case study

--- case_study/project/src/components/contact/ContactCreate.js
+++ case_study/project/src/components/contact/ContactCreate.js
@@ -22,11 +22,11 @@ export function ContactCreate() {
                     }}
 
                     validationSchema={Yup.object({
-                        contactCode: Yup.string().required("Not empty"),
+                        contactCode: Yup.string().required("Not empty").matches('^C(0{3})[0-9]+$', "Not contactcode"),
                         startDate: Yup.date().required("Not empty").min(new Date(), "Greater than now"),
                         endDate: Yup.date().required("Not empty").min(Yup.ref('startDate'), "End date can't be before Start date"),
                         deposit: Yup.number("Not number").nullable().required("Not Empty").moreThan(0, "Greater than zero"),
-                        totalPrice: Yup.number().required("Not empty").min(Yup.ref('deposit'),"Totalprice can't be less than deposit"),
+                        totalPrice: Yup.number().required("Not empty").min(Yup.ref('deposit'), "Totalprice can't be less than deposit"),
                     })}
 
 
@@ -39,7 +39,7 @@ export function ContactCreate() {
                         <div className="form-floating mb-3">
                             <Field className="form-control" name="contactCode" id="contactCode" type="text" placeholder="Contact Code" data-sb-validations="required" />
                             <label htmlFor="contactCode">Contact Code</label>
-                            <div className="invalid-feedback" data-sb-feedback="contactCode:required">Contact Code is required.</div>
+                            <div style={{ color: 'red' }}><ErrorMessage component="span" name='contactCode' /></div>
                         </div>
                         <div className="form-floating mb-3">
                             <Field className="form-control" name="startDate" id="startDate" type="date" placeholder="Start Date" data-sb-validations />
@@ -60,9 +60,6 @@ export function ContactCreate() {
                             <Field className="form-control" id="totalPrice" name="totalPrice" type="number" placeholder="Total Price" data-sb-validations />
                             <label htmlFor="totalPrice">Total Price</label>
                             <div style={{ color: 'red' }}><ErrorMessage component="span" name='totalPrice' /></div>
-                        </div>
-                        <div className="d-none" id="submitErrorMessage">
-                            <div className="text-center text-danger mb-3">Error sending message!</div>
                         </div>
                         <div className="d-grid">
                             <button className="btn btn-primary btn-lg" id="submitButton" type="submit">Create</button>
